e8bf22821b056c34bd650b4a08221c023414be62
sota <git@xsota.com>
2016-05-21 21:27:28 +0900

    first commit

---
+++ android/src/hoge/libgdx/test/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hoge.libgdx.test;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hoge.libgdx.test.GdxTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxTest(), config);
+	}
+}
